e34aa57c5b74cc44563dfb7244edc2a85542fa4c
wrinkl3 <al.seward@gmail.com>
2016-06-25 02:00:31 +0400

    init sim project

---
+++ test/PingServiceTest.java
@@ -0,0 +1,25 @@
+import ge.edu.freeuni.sdp.todo.core.PingService;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+
+public class PingServiceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(PingService.class);
+    }
+
+    @Test
+    public void ping() throws Exception {
+        Response response = target("ping").request().get();
+        assertEquals(200, response.getStatus());
+    }
+
+}
