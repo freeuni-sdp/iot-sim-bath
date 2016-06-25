fa0568e605fd4b2192dd5e0ab39d1a67b4820cdf
gosip <gosip12@freeuni.edu.ge>
2016-06-25 19:52:55 +0400

    Bathroom turned into singleton.

--- src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/Bathroom.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/Bathroom.java
@@ -2,13 +2,36 @@ package ge.edu.freeuni.sdp.iot.simulator.bath.model;
 
 public class Bathroom {
 
+    private static Bathroom instance;
+    public static String TEST_HOUSE_ID = "0";
+
+    public static Bathroom getInstance() {
+        if (instance == null) {
+            instance = new Bathroom();
+            instance.setLightSwitch(new LightSwitch(TEST_HOUSE_ID));
+            instance.setHumiditySensor(new HumiditySensor(TEST_HOUSE_ID));
+            instance.setVentSwitch(new VentSwitch(TEST_HOUSE_ID));
+        }
+        return instance;
+    }
+
     private LightSwitch lightSwitch;
     private HumiditySensor humiditySensor;
     private VentSwitch ventSwitch;
 
-    public Bathroom(LightSwitch lightSwitch, HumiditySensor humiditySensor, VentSwitch ventSwitch) {
-        this.lightSwitch = lightSwitch;
+    private Bathroom() {
+
+    }
+
+    private void setHumiditySensor(HumiditySensor humiditySensor) {
         this.humiditySensor = humiditySensor;
+    }
+
+    private void setLightSwitch(LightSwitch lightSwitch) {
+        this.lightSwitch = lightSwitch;
+    }
+
+    private void setVentSwitch(VentSwitch ventSwitch) {
         this.ventSwitch = ventSwitch;
     }
 
