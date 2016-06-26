91c3889ff1a68f45ef43434054af0aa4a8a91a10
wrinkl3 <al.seward@gmail.com>
2016-06-27 00:18:38 +0400

    Implement posting to light sensor from light switch

--- src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/LightSwitch.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/LightSwitch.java
@@ -1,5 +1,12 @@
 package ge.edu.freeuni.sdp.iot.simulator.bath.model;
 
+import ge.edu.freeuni.sdp.iot.simulator.bath.jaxb.MyJaxBean;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 public class LightSwitch {
     private String houseId;
 
@@ -9,9 +16,19 @@ public class LightSwitch {
 
     public void lightOn() {
         //TODO send post request to https://iot-bath-light-sensor.herokuapp.com/
+        WebTarget target = ClientBuilder.newClient().target("http://iot-bath-light-sensor.herokuapp.com/webapi/houses/" + houseId);
+        MyJaxBean jaxb = new MyJaxBean();
+        jaxb.houseId = houseId;
+        jaxb.status = "on";
+        target.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(jaxb));
     }
 
     public void lightOff() {
         //TODO send post request to https://iot-bath-light-sensor.herokuapp.com/
+        WebTarget target = ClientBuilder.newClient().target("http://iot-bath-light-sensor.herokuapp.com/webapi/houses/" + houseId);
+        MyJaxBean jaxb = new MyJaxBean();
+        jaxb.houseId = houseId;
+        jaxb.status = "off";
+        target.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(jaxb));
     }
 }
