aa68e9892044a8f227be5cb4187d1d7f6311ada4
khrak <mamuka_sakhelashvili@yahoo.com>
2016-06-27 08:24:37 +0400

    add lowercase actions for compariso

--- src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/Bathroom.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/model/Bathroom.java
@@ -2,17 +2,17 @@ package ge.edu.freeuni.sdp.iot.simulator.bath.model;
 
 public class Bathroom {
 
-    private static Bathroom instance;
+    public static Bathroom instance;
     public static String TEST_HOUSE_ID = "a1bc00fb-4724-476a-a553-d65d2c1bac0e";
 
     public static Bathroom getInstance() {
-        if (instance == null) {
-            instance = new Bathroom();
-            instance.setLightSwitch(new LightSwitch(TEST_HOUSE_ID));
-            instance.setHumiditySensor(new HumiditySensor(TEST_HOUSE_ID));
-            instance.setVentSwitch(new VentSwitch(TEST_HOUSE_ID));
+        if (Bathroom.instance == null) {
+            Bathroom.instance = new Bathroom();
+            Bathroom.instance.setLightSwitch(new LightSwitch(TEST_HOUSE_ID));
+            Bathroom.instance.setHumiditySensor(new HumiditySensor(TEST_HOUSE_ID));
+            Bathroom.instance.setVentSwitch(new VentSwitch(TEST_HOUSE_ID));
         }
-        return instance;
+        return Bathroom.instance;
     }
 
     private LightSwitch lightSwitch;
@@ -20,7 +20,7 @@ public class Bathroom {
     private VentSwitch ventSwitch;
 
     private Bathroom() {
-        
+
     }
 
     private void setHumiditySensor(HumiditySensor humiditySensor) {
