516bb78fb8cb7448f7a9dd61d512feba18425136
Giorgi <gkink12@freeuni.edu.ge>
2016-06-27 04:14:19 +0400

    Bathroom service bug fixed

--- src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/service/BathroomService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulator/bath/service/BathroomService.java
@@ -5,6 +5,7 @@ import ge.edu.freeuni.sdp.iot.simulator.bath.model.Bathroom;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/bath")
 public class BathroomService {
@@ -12,12 +13,15 @@ public class BathroomService {
     @POST
     @Path("/light-switch")
     @Consumes({MediaType.APPLICATION_JSON})
-    public void switchLight(final LightSwitchPostRequest request) {
-        if (request.status.equals("ON")) {
+    public Response switchLight(final LightSwitchPostRequest request) {
+        if (request.getStatus().equals("ON")) {
             Bathroom.getInstance().turnLightOn();
+            return Response.ok().build();
         }
-        else if (request.status.equals("OFF")) {
+        else if (request.getStatus().equals("OFF")) {
             Bathroom.getInstance().turnLightOff();
+            return Response.ok().build();
         }
+        return null;
     }
 }
